9608fec83d215f4c95e45d524e28d28f6f37d1f3
Ros McMahon <rosmcmahon@gmail.com>
2020-02-09 00:17:24 +0000

    take photos and saving to memory cached gallery

---
+++ src/hooks/usePhotoGallery.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { useFilesystem, base64FromPath } from '@ionic/react-hooks/filesystem';
+import { useStorage } from '@ionic/react-hooks/storage';
+import { isPlatform } from '@ionic/react';
+import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
+import { useCamera } from '@ionic/react-hooks/camera';
+
+
+
+export function usePhotoGallery() {
+	const { getPhoto } = useCamera();
+	const [photos, setPhotos] = useState<Photo[]>([]);
+
+  const takePhoto = async () => {
+    const cameraPhoto = await getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+		});
+		const fileName = new Date().getTime() + '.jpeg';
+		const newPhotos = [{
+			filepath: fileName,
+			webviewPath: cameraPhoto.webPath
+		}, ...photos];
+		setPhotos(newPhotos)
+  };
+
+  return {
+		photos,
+    takePhoto
+  };
+}
+
+export interface Photo {
+  filepath: string;
+  webviewPath?: string;
+  base64?: string;
+}
